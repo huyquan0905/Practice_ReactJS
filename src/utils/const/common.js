91163170f167313405dc46b76b540566bdc11a24
QuanHH <huyquanwork95@gmail.com>
2024-11-26 15:00:32 +0700

    push project todo list

---
+++ src/utils/const/common.js
@@ -0,0 +1,8 @@
+export const LB_TO_DO_LIST = "ToDo List";
+export const LB_ADD = "Add";
+export const MSG_ADD_TO_DO_LIST = "Add to do list?";
+export const LB_All = "All";
+export const LB_Active = "Active";
+export const LB_Complete = "Complete";
+export const LB_CLEAR_COMPLETED = "Clear Completed";
+export const LB_TOTAL_ITEM = "Total item: ";
