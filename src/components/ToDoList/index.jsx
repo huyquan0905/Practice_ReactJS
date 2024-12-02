5525dd94b3f1f850abed944a656a4b43abb47e76
QuanHH <huyquanwork95@gmail.com>
2024-12-02 20:44:38 +0700

    add paging component

--- src/components/ToDoList/index.jsx
+++ src/components/ToDoList/index.jsx
@@ -3,11 +3,16 @@ import InputField from './InputField';
 import ToDoItem from './ToDoItem';
 import FilterItem from './FilterItem';
 import { LB_STATUS, LB_TO_DO_LIST } from '../../utils/const/common';
+import { TablePagination } from '@mui/material';
 
 const ToDoPage = () => {
     const ListJob = "JOB_LIST";
     const [job, setJob] = useState("");
     const [filter, setFilter] = useState(LB_STATUS.ALL);
+
+    const [page, setPage] = useState(0);
+    const [rowsPerPage, setRowsPerPage] = useState(5);
+
     const jobListRef = useRef(() => {
         const storageJobList = JSON.parse(localStorage.getItem(ListJob));
         return storageJobList ?? [];
@@ -64,6 +69,21 @@ const ToDoPage = () => {
         return true;
     });
 
+    const paginatedJobs = filteredJobs.slice(
+        page * rowsPerPage,
+        page * rowsPerPage + rowsPerPage
+    );
+
+    const handleChangePage = (event, newPage) => {
+        setPage(newPage);
+    };
+
+    const handleChangeRowsPerPage = (event) => {
+        setRowsPerPage(event.target.value);
+        setPage(0);
+    };
+
+
     return (
         <div style={{ padding: "10px", textAlign: "center" }}>
             <h1 style={{ color: "#b83f45" }}>{LB_TO_DO_LIST}</h1>
@@ -72,8 +92,9 @@ const ToDoPage = () => {
                 setJob={setJob}
                 handleAddJob={handleAddJob}
             />
+
             <ul style={{ listStyleType: "none", padding: 0, margin: "20px auto", maxWidth: "200px" }}>
-                {filteredJobs.map((job) => (
+                {paginatedJobs.map((job) => (
                     <ToDoItem
                         key={job.id}
                         job={job}
@@ -82,6 +103,18 @@ const ToDoPage = () => {
                     />
                 ))}
             </ul>
+
+            <TablePagination
+                sx={{ display: "flex", justifyContent: "center", marginTop: "20px" }}
+                rowsPerPage={rowsPerPage}
+                rowsPerPageOptions={[5, 10, 15]}
+                count={filteredJobs.length}
+                page={page}
+                onPageChange={handleChangePage}
+                onRowsPerPageChange={handleChangeRowsPerPage}
+            />
+
+
             <FilterItem
                 filter={filter}
                 setFilter={setFilter}
